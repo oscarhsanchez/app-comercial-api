540690f0cada52b5006efc409147215d37e320e9
Jaime Banus <jaime.banus@rbconsulting.es>
2016-04-26 10:17:05 +0200

    + subtipos en medios.

--- application/models/medios/medio_model.php
+++ application/models/medios/medio_model.php
@@ -14,6 +14,39 @@ require_once(APPPATH.GENERIC_MODEL);
  */
 class medio_model extends generic_Model {
 
+    function __construct() {
+        parent::__construct();
+        $this->load->model('medios/subtipo_medio_model');
+
+    }
+
+    function getAll($get_vars, $countryId=0, $offset, $limit, $sort, $pagination) {
+        $extended = null;
+        if (isset($get_vars["extended"])) {
+            $extended = $get_vars["extended"];
+            unset($get_vars["extended"]);
+        }
+
+        $result = parent::getAll($get_vars, $countryId, $offset, $limit, $sort, $pagination);
+
+        if ($extended) {
+            $mediosArr = array();
+
+            foreach ($result AS $medio) {
+                $subtipo = $this->subtipo_medio_model->getBy("pk_subtipo", $medio->fk_subtipo, $countryId);
+                $medio->subtipo = $subtipo;
+
+                $mediosArr[] = $medio;    
+            }
+
+            $result["result"] = $mediosArr;
+
+            return $result;
+
+        } else
+            return $result;
+
+    }
 	
 }
 
