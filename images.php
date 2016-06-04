02703d5faaea42e22e5cc0939d730e37a4a456c7
Jaime Banus <jaime.banus@rbconsulting.es>
2016-06-04 11:03:56 +0200

    Listamos arhivo de diretorio de ubicaciones

--- images.php
+++ images.php
@@ -1,6 +1,6 @@
 <?php
 
-if ($handle = opendir('.')) {
+if ($handle = opendir('/var/www/vhosts/vallas-admin/shared/web/media/ubicacion_imagen')) {
 
     while (false !== ($entry = readdir($handle))) {
 
