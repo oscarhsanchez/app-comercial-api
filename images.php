b505a0f7389f91c09b87549dfdcbe6a55a5a02fe
Jaime Banus <jaime.banus@rbconsulting.es>
2016-06-04 10:56:55 +0200

    + Prueba de importacion de imagenes

---
+++ images.php
@@ -0,0 +1,16 @@
+<?php
+
+if ($handle = opendir('.')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry\n";
+        }
+    }
+
+    closedir($handle);
+}
+
+?>
