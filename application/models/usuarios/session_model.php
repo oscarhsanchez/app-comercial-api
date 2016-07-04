640ea2a8e49b618993ade3e7578e17f20a57ed2a
Jaime Banus <jaime.banus@rbconsulting.es>
2016-07-04 14:01:10 +0200

    Quitamos echos.

--- application/models/usuarios/session_model.php
+++ application/models/usuarios/session_model.php
@@ -14,9 +14,7 @@ class session_model extends CI_Model {
     function createSession($userId, $codigo, $roles, $countryId, $phoneId) {
 		$token = sha1($userId.$countryId.time().TOKEN_SALT_KEY);
         $renew_token = sha1($userId.$countryId.time().RENEW_SALT_KEY);
-        echo "hora: " . time();
-        echo " token:" . $token;
-        echo " datos: " .$userId.$countryId.time().TOKEN_SALT_KEY;
+        
 		$sesion = new Session();
         $sesion->fk_user = $userId;
         $sesion->fk_pais = $countryId;
